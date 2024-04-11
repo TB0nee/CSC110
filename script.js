bbdef0c4c8d95c0632518ff75fb6d0ef8e091a23
TB0nee <165042310+TB0nee@users.noreply.github.com>
2024-04-11 19:55:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function menuClick(newshow) {
+  console.log(newshow)
+  document.getElementsByClassName('show')[0].className='noshow';
+  // show the one dive that you sent to this function
+  if (newshow==="show1"){
+     document.getElementById(newshow).className="Video show";
+  } else if  (newshow==="show2"){
+     document.getElementById(newshow).className="NCQAContent show";
+  } else if  (newshow==="show3"){
+     document.getElementById(newshow).className="testimonialContent show";
+  } else if  (newshow==="show4"){
+     document.getElementById(newshow).className="AboutUsContent show";
+  } else if  (newshow==="show5"){
+    document.getElementById(newshow).className="contactUsContent show";
+  }
+}
+
+
+
 
